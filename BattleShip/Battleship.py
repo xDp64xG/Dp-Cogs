5a5fe6a80069f188ad7553c64425b6d262cd9060
DESKTOP-6723BCQ\browp <brow.paul64@gmail.com>
2017-10-04 13:51:13 -0400

    Updated code. New grid, instructions, author, etc.

--- BattleShip/Battleship.py
+++ BattleShip/Battleship.py
@@ -4,11 +4,8 @@ from random import randint
 
 class Battleship:
 
-
     def __init__(self, bot):
         self.bot = bot
-
-
 
     @commands.command(pass_context=True)
     async def battleship(self, ctx):
@@ -20,10 +17,53 @@ class Battleship:
         msg = ""
         msg2 = ""
         total = 0
+        check = 0
         board = []
         seperate = []
         author = ctx.message.author
         channel = ctx.message.channel
+
+        embed=discord.Embed(
+            title="About Battleship", 
+            description="~ A simple game of Battle Ships built into Magik Bot.\nO - Open Target\nX - Missed Target\nS = Target Hit\nM = Location of ships (at the end of the game)", 
+            color=0x207cee)
+        embed.set_author(
+            name="Magik bot", url='http://www.magikbot.co.uk', 
+            icon_url='https://cdn.discordapp.com/attachments/355249562719617024/357107055691169797/MB_Icon.png')
+        embed.set_thumbnail(
+            url='https://cdn.discordapp.com/attachments/355249562719617024/365100412874784768/Battleship-ubicom-VIDEO-launch_trailer_2016_08_02-712x712_Desktop_261122.png')
+        embed.add_field(
+            name="How to play", 
+            value="Enter your X and Y value as a comment like this `4 2`. No prefix required. Type `cancel` to stop the game.", 
+            inline=True)
+        embed.add_field(
+            name="How many turns", 
+            value="You have 9 attempts to hit my 4 ships", 
+            inline=True)
+        embed.add_field(
+            name="Auther", 
+            value="UnseenMagik & Potato", 
+            inline=True)
+        embed.add_field(
+            name="Battleship Board Layout",
+            value=("```Y  ─── ─── ─── ─── ─── ─── \n"
+                   "  │ 5 │ O │ O │ O │ O │ O │\n"
+                   "   ─── ─── ─── ─── ─── ─── \n"
+                   "  │ 4 │ O │ O │ O │ O │ O │\n"
+                   "   ─── ─── ─── ─── ─── ─── \n"
+                   "A │ 3 │ O │ O │ O │ O │ O │\n"
+                   "   ─── ─── ─── ─── ─── ─── \n"
+                   "X │ 2 │ O │ O │ O │ O │ O │\n"
+                   "   ─── ─── ─── ─── ─── ─── \n"
+                   "I │ 1 │ O │ O │ O │ O │ O │\n"
+                   "   ─── ─── ─── ─── ─── ─── \n"
+                   "s │ 0 │ 1 │ 2 │ 3 │ 4 │ 5 │\n"
+                   "   ─── ─── ─── ─── ─── ─── \n"
+                   "    X       A   X   I   S```"), inline=True)
+        embed.set_footer(
+            text="Magik Bot - Providing Discord support since September 2017")
+        
+        await self.bot.say(embed=embed)
 
 
         guide = """
@@ -51,26 +91,27 @@ O O O O O 5 I
         """
 
         for x in range(5): #Size of the board
-            board.append(["O"] * 5)
+            board.append(["⭕"] * 5)
 
         def print_board(board): #Making the board
-            i = "```"
+            i = "```\n"
             for x in board:
                 i = i + " ".join(x)+"\n"
+                #print(len(x))
             i += "```"
             return i
 
-        await self.bot.say(guide)
+        #await self.bot.say(guide)
         print ("Let's play Battleship!")
         print (" ")
         await self.bot.say("Let's play Battleship!"+ "\n")
         await self.bot.say(print_board(board))
 
         def random_x(board):
-            return randint(1, len(board) - 1)
+            return randint(0, len(board) - 1)
 
         def random_y(board):
-            return randint(1, len(board) - 1)
+            return randint(0, len(board) - 1)
 
         #ship(num)a is equal to x
         #ship(num)b is equal to y
@@ -87,7 +128,7 @@ O O O O O 5 I
 
         if ship1a == 0:
             ship1d = ship1a + 1
-        elif ship1a == 5:
+        elif ship1a == 4:
             ship1d = ship1a - 1
         else:
             ship1d = ship1a + 1
@@ -110,26 +151,56 @@ O O O O O 5 I
         l = len(board)
         
         #For debugging purposes
-        print("Ship 1: ", ship_x, ship_y)
-        print("Ship 2: ", ship1a, ship1b, " ", ship1d, ship1b)
-        print("Ship 3: ", ship2a, ship2b, " ", ship2a, ship2c)
+        print("Ship 1: ", ship_x+1, ship_y+1)
+        print("Ship 2: ", ship1a+1, ship1b, " ", ship1d+1, ship1b)
+        print("Ship 3: ", ship2a+1, ship2b, " ", ship2a+1, ship2c)
         
 
         for turn in range(10):
-            
+            print("Turn before: ",str(turn))
+            """try:
+        n = raw_input("Please enter an integer: ")
+        n = int(n)
+        break
+    except ValueError:
+        print("No valid integer! Please try again ...")"""
+
+            guess_x = -1
+            guess_y = -1
+
             await self.bot.say("\n"+"Guess X and Y:")
+            
             msg = await self.bot.wait_for_message(author=author, channel=channel)
 
             if msg.content == "Cancel" or msg.content == "cancel":
                 await self.bot.say("Stopping game.")
                 print("Stopping the game.")
                 break
-            
-            msg2 = msg.content
-            seperate = msg2.split(" ")
 
-            guess_x = int(seperate[1]) - 1 
-            guess_y = int(seperate[0]) - 1
+            try:
+                check = 0
+                msg2 = msg.content
+                seperate = msg2.split(" ")
+
+                guess_x = int(seperate[1]) - 1 
+                guess_y = int(seperate[0]) - 1
+
+            except IndexError:
+                await self.bot.say("Error. Invalid response.")
+                print("Invalid Response. IndexError")
+                turn -= 1
+            except ValueError:
+                turn -= 1
+                await self.bot.say("Error. Invalid response.")
+                print("Invalid Response. ValueError")
+            except UnboundLocalError:
+                turn -= 1
+                print("Invalid. UnboundLocalError")
+                
+
+            
+            
+            
 
             if total == 4:
 
@@ -144,7 +215,7 @@ O O O O O 5 I
             
             elif guess_x == ship_x and guess_y == ship_y:
 
-                board[guess_x][guess_y] = "S"
+                board[guess_x][guess_y] = "🚢"
                 print("Sunk a ship.")
                 await self.bot.say("You sunk a battleship!")
                 await self.bot.say(print_board(board))
@@ -153,7 +224,7 @@ O O O O O 5 I
             #-------------------------------------------#    
             elif guess_x == ship1a and guess_y == ship1b:
 
-                board[guess_x][guess_y] = "S"
+                board[guess_x][guess_y] = "🚢"
 
                 if num == 0:
 
@@ -171,7 +242,7 @@ O O O O O 5 I
 
             elif guess_x == ship1d and guess_y == ship1b:
 
-                board[guess_x][guess_y] = "S"
+                board[guess_x][guess_y] = "🚢"
 
                 if num == 0:
 
@@ -189,7 +260,7 @@ O O O O O 5 I
                 #-----------------------------------------------#
             elif guess_x == ship2a and guess_y == ship2b:
 
-                board[guess_x][guess_y] = "S"
+                board[guess_x][guess_y] = "🚢"
 
                 if num2 == 0:
 
@@ -207,7 +278,7 @@ O O O O O 5 I
 
             elif guess_x == ship2a and guess_y == ship2c:
 
-                board[guess_x][guess_y] = "S"
+                board[guess_x][guess_y] = "🚢"
 
                 if num2 == 0:
 
@@ -224,21 +295,24 @@ O O O O O 5 I
 
                 total += 1
                 #--------------------------------------#
-                
+            
+            elif check != 0:
+                turn -= 1
+
             else:
                 if (guess_x < 0 or guess_x > l-1) or (guess_y < 0 or guess_y > l-1):
 
                     await self.bot.say("Oops, that's not even in the ocean.")
                     print ("Oops, that's not even in the ocean.")
                     
-                elif(board[guess_x][guess_y] == "X"):
+                elif(board[guess_x][guess_y] == "❌"):
 
                     await self.bot.say("You guessed that one already.")
                     print ("You guessed that one already.")
                 else:
 
                     print ("You missed my battleship!")
-                    board[guess_x][guess_y] = "X"
+                    board[guess_x][guess_y] = "❌"
                     await self.bot.say("You missed my battleship! ")
 
                     if turn == 9:
@@ -253,6 +327,7 @@ O O O O O 5 I
                         print("Here are all the ships, they're labeled M.")
 
                 await self.bot.say(print_board(board))
+                print("Turn after: ",str(turn))
 
 
         #----------------------------------------------------------------#
