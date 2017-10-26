f29f530410c0ed981ef89bf5e7578753b1121a03
xDp64xG <brow.paul64@gmail.com>
2017-10-26 10:09:00 -0400

    Update Battleship.py

--- Battleship/Battleship.py
+++ Battleship/Battleship.py
@@ -69,12 +69,12 @@ class Battleship:
         embed.add_field(
             name="Battleship Board Layout",
             value=(""":regional_indicator_y: :one: :zero: :zero: :zero: :zero: :zero:
-:gap: :two: :zero: :zero: :zero: :zero: :zero:
+<:gap:373109551009890304> :two: :zero: :zero: :zero: :zero: :zero:
 :regional_indicator_a: :three: :zero: :zero: :zero: :zero: :zero:
 :regional_indicator_x: :four: :zero: :zero: :zero: :zero: :zero:
 :regional_indicator_i: :five: :zero: :zero: :zero: :zero: :zero:
 :regional_indicator_s: :zero: :one: :two: :three: :four: :five:
-:gap: :regional_indicator_x: :gap: :regional_indicator_a: :regional_indicator_x: :regional_indicator_i: :regional_indicator_s:"""), inline=True)
+<:gap:373109551009890304> :regional_indicator_x: <:gap:373109551009890304> :regional_indicator_a: :regional_indicator_x: :regional_indicator_i: :regional_indicator_s:"""), inline=True)
         embed.set_footer(
             text="Magik Bot - Providing Discord support since September 2017")
         
