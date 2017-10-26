20c70c90e60e8644940adf9ea9db17a0b00d24a8
xDp64xG <brow.paul64@gmail.com>
2017-10-26 09:04:13 -0400

    Update Battleship.py

--- Battleship/Battleship.py
+++ Battleship/Battleship.py
@@ -243,7 +243,6 @@ class Battleship:
             elif guess_x == ship_x and guess_y == ship_y:
                 #Experimental
                 if board[guess_x][guess_y] == ":large_blue_circle:":
-                    await self.bot.say("Turns shouldn't increase here")
                     turn += 1
                     turn2 -= 1
                 else:
@@ -256,7 +255,6 @@ class Battleship:
             #-------------------------------------------#    
             elif guess_x == ship1a and guess_y == ship1b:
                 if board[guess_x][guess_y] == ":large_blue_circle:":
-                    await self.bot.say("Turns shouldn't increase here(2)")
                     turn += 1
                     turn2 -= 1
                 else:
@@ -273,7 +271,6 @@ class Battleship:
 
             elif guess_x == ship1d and guess_y == ship1b:
                 if board[guess_x][guess_y] == ":large_blue_circle:":
-                    await self.bot.say("Turns shouldn't increase here(3)")
                     turn += 1
                     turn2 -= 1
                 else:
@@ -291,7 +288,6 @@ class Battleship:
                 #-----------------------------------------------#
             elif guess_x == ship2a and guess_y == ship2b:
                 if board[guess_x][guess_y] == ":large_blue_circle:":
-                    await self.bot.say("Turns shouldn't increase here(4)")
                     turn += 1
                     turn2 -= 1
                 else:
@@ -309,7 +305,6 @@ class Battleship:
 
             elif guess_x == ship2a and guess_y == ship2c:
                 if board[guess_x][guess_y] == ":large_blue_circle:":
-                    await self.bot.say("Turns shouldn't increase here(5)")
                     turn += 1
                     turn2 -= 1
                 else:
