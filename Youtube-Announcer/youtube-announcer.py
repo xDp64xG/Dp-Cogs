c63e0af21b6f8a7a8f9cfa543a81c33b92a6e272
xDp64xG <brow.paul64@gmail.com>
2017-11-07 15:15:58 -0500

    Update youtube-announcer.py

--- Youtube-Announcer/youtube-announcer.py
+++ Youtube-Announcer/youtube-announcer.py
@@ -1,1 +1,18 @@
-print("Hi")
+import discord
+from discord.ext import commands
+#Change Mycog to something easier to use the command?#
+class Mycog: #Here
+    """My custom cog that does stuff!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def mycom(self):
+        """This does stuff!"""
+
+        #Your code will go here
+        await self.bot.say("I can do stuff!")
+
+def setup(bot):
+    bot.add_cog(Mycog(bot)) #Here
