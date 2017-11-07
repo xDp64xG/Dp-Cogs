99876e128344fc18893039489d5be90e5ca9988e
xDp64xG <brow.paul64@gmail.com>
2017-11-07 15:59:24 -0500

    Update youtube-announcer.py

--- Youtube-Announcer/youtube-announcer.py
+++ Youtube-Announcer/youtube-announcer.py
@@ -1,5 +1,23 @@
 import discord
 from discord.ext import commands
+
+import gdata.youtube
+import gdata.youtube.service
+
+yt_service = gdata.youtube.service.YouTubeService()
+
+# Turn on HTTPS/SSL access.
+# Note: SSL is not available at this time for uploads.
+yt_service.ssl = True
+
+yt_service.developer_key = 'AIzaSyAie7tXECe8LZlvVfQGeq-pyj3G3J4hHM0'
+yt_service.client_id = 'youtube-api-185317'
+
+def GetAndPrintUserUploads(username):
+  yt_service = gdata.youtube.service.YouTubeService()
+  uri = 'http://gdata.youtube.com/feeds/api/users/%s/uploads' % username
+  PrintVideoFeed(yt_service.GetYouTubeVideoFeed(uri))
+
 #Change Mycog to something easier to use the command?#
 class Mycog: #Here
     """My custom cog that does stuff!"""
@@ -12,6 +30,9 @@ class Mycog: #Here
         """This does stuff!"""
 
         #Your code will go here
+        username = GrimBOMB
+        printThis = GetAndPrintUserUploads(username)
+        await self.bot.say(printThis)
         await self.bot.say("I can do stuff!")
 
 def setup(bot):
