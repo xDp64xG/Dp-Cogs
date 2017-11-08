a1b43bd6c1b305a0605ba54b6f2925d120b41d3a
xDp64xG <brow.paul64@gmail.com>
2017-11-08 15:39:11 -0500

    Update Youtube-Announcer.py

--- Youtube-Announcer/Youtube-Announcer.py
+++ Youtube-Announcer/Youtube-Announcer.py
@@ -1,22 +1,22 @@
 import discord
 from discord.ext import commands
 
-import gdata.youtube
-import gdata.youtube.service
+#import gdata.youtube
+#import gdata.youtube.service
 
-yt_service = gdata.youtube.service.YouTubeService()
+#yt_service = gdata.youtube.service.YouTubeService()
 
 # Turn on HTTPS/SSL access.
 # Note: SSL is not available at this time for uploads.
-yt_service.ssl = True
+#yt_service.ssl = True
 
-yt_service.developer_key = 'AIzaSyAie7tXECe8LZlvVfQGeq-pyj3G3J4hHM0'
-yt_service.client_id = 'youtube-api-185317'
+#yt_service.developer_key = 'AIzaSyAie7tXECe8LZlvVfQGeq-pyj3G3J4hHM0'
+#yt_service.client_id = 'youtube-api-185317'
 
-def GetAndPrintUserUploads(username):
-  yt_service = gdata.youtube.service.YouTubeService()
-  uri = 'http://gdata.youtube.com/feeds/api/users/%s/uploads' % username
-  PrintVideoFeed(yt_service.GetYouTubeVideoFeed(uri))
+#def GetAndPrintUserUploads(username):
+  #yt_service = gdata.youtube.service.YouTubeService()
+  #uri = 'http://gdata.youtube.com/feeds/api/users/%s/uploads' % username
+  #PrintVideoFeed(yt_service.GetYouTubeVideoFeed(uri))
 
 #Change Mycog to something easier to use the command?#
 class Mycog: #Here
@@ -30,10 +30,11 @@ class Mycog: #Here
         """This does stuff!"""
 
         #Your code will go here
-        username = GrimBOMB
-        printThis = GetAndPrintUserUploads(username)
-        await self.bot.say(printThis)
-        await self.bot.say("I can do stuff!")
+        username = 'GrimBOMB'
+        #printThis = GetAndPrintUserUploads(username)
+        #await self.bot.say(printThis)
+        await self.bot.say(username)
+        #await self.bot.say("I can do stuff!")
 
 def setup(bot):
     bot.add_cog(Mycog(bot)) #Here
