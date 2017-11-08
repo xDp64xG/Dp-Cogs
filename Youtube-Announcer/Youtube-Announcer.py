411a4802fef9c7774f75f33d6f98dbec1150ba9c
xDp64xG <brow.paul64@gmail.com>
2017-11-08 17:13:31 -0500

    Create Youtube-Announcer.py

---
+++ Youtube-Announcer/Youtube-Announcer.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+try: # check if BeautifulSoup4 is installed
+	from bs4 import BeautifulSoup
+	soupAvailable = True
+except:
+	soupAvailable = False
+
+class YTAnnouncer:
+    """My custom cog that does stuff!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def update(self):
+        """This does stuff!"""
+
+        #Your code will go here
+        await self.bot.say("I can do stuff!")
+		await self.bot.say(soupAvailable)
+
+def setup(bot):
+	bot.add_cog(YTAnnouncer(bot))
+
