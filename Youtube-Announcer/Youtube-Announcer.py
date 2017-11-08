f438a1e27a4fffbf771d230b3da5011d7eaf8c5d
xDp64xG <brow.paul64@gmail.com>
2017-11-08 16:59:48 -0500

    Update Youtube-Announcer.py

--- Youtube-Announcer/Youtube-Announcer.py
+++ Youtube-Announcer/Youtube-Announcer.py
@@ -1,29 +1,23 @@
 import discord
 from discord.ext import commands
 
-
-
-class Youtube_Announcer:
+class Mycog:
     """My custom cog that does stuff!"""
 
     def __init__(self, bot):
         self.bot = bot
-   
-      
+
     @commands.command()
-    async def update(self):
-	try: # check if BeautifulSoup4 is installed
-		from bs4 import BeautifulSoup
-		soupAvailable = True
-	except:
-		soupAvailable = False
-		#Start
+    async def mycom(self):
+		try: # check if BeautifulSoup4 is installed
+			from bs4 import BeautifulSoup
+			soupAvailable = True
+		except:
+			soupAvailable = False
         """This does stuff!"""
 
         #Your code will go here
-        username = 'GrimBOMB'
-       	await self.bot.say(str(soupAvailable))
-        #await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
 
 def setup(bot):
     if soupAvailable:
