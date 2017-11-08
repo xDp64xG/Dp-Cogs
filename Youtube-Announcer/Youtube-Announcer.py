87cbf2e55222ab0d95d3d5739e357e8c8b0b9050
xDp64xG <brow.paul64@gmail.com>
2017-11-08 16:44:07 -0500

    Update Youtube-Announcer.py

--- Youtube-Announcer/Youtube-Announcer.py
+++ Youtube-Announcer/Youtube-Announcer.py
@@ -41,8 +41,8 @@ with information from the {{ Cloud Console }}
 
 For more information about the client_secrets.json file format, please visit:
 https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
-""" % os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                   CLIENT_SECRETS_FILE))"""
+# % os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                   CLIENT_SECRETS_FILE))
 #import gdata.youtube
 #import gdata.youtube.service
 
