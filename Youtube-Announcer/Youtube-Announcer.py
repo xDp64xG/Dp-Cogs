775374ead0d434d35c782bce27217035e021ef97
xDp64xG <brow.paul64@gmail.com>
2017-11-08 16:00:06 -0500

    Update Youtube-Announcer.py
    Starting to work out the function now, base is done.

--- Youtube-Announcer/Youtube-Announcer.py
+++ Youtube-Announcer/Youtube-Announcer.py
@@ -1,6 +1,48 @@
 import discord
 from discord.ext import commands
 
+
+#!/usr/bin/python
+
+import httplib2
+import os
+import sys
+
+from apiclient.discovery import build
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.file import Storage
+from oauth2client.tools import argparser, run_flow
+
+
+# The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
+# the OAuth 2.0 information for this application, including its client_id and
+# client_secret. You can acquire an OAuth 2.0 client ID and client secret from
+# the {{ Google Cloud Console }} at
+# {{ https://cloud.google.com/console }}.
+# Please ensure that you have enabled the YouTube Data API for your project.
+# For more information about using OAuth2 to access the YouTube Data API, see:
+#   https://developers.google.com/youtube/v3/guides/authentication
+# For more information about the client_secrets.json file format, see:
+#   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+CLIENT_SECRETS_FILE = "client_secrets.json"
+
+# This variable defines a message to display if the CLIENT_SECRETS_FILE is
+# missing.
+MISSING_CLIENT_SECRETS_MESSAGE = """
+WARNING: Please configure OAuth 2.0
+
+To make this sample run you will need to populate the client_secrets.json file
+found at:
+
+   %s
+
+with information from the {{ Cloud Console }}
+{{ https://cloud.google.com/console }}
+
+For more information about the client_secrets.json file format, please visit:
+https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+""" % os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                   CLIENT_SECRETS_FILE))
 #import gdata.youtube
 #import gdata.youtube.service
 
@@ -18,13 +60,15 @@ from discord.ext import commands
   #uri = 'http://gdata.youtube.com/feeds/api/users/%s/uploads' % username
   #PrintVideoFeed(yt_service.GetYouTubeVideoFeed(uri))
 
-#Change Mycog to something easier to use the command?#
+
 class Youtube_Announcer: #Here
     """My custom cog that does stuff!"""
 
     def __init__(self, bot):
         self.bot = bot
-
+    def youtube(username):
+      pass
+      
     @commands.command()
     async def update(self):
         """This does stuff!"""
