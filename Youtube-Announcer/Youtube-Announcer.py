be6c2e01ef57580178594c36d66ed6e0db4c99e1
xDp64xG <brow.paul64@gmail.com>
2017-11-09 01:32:44 -0500

    Update Youtube-Announcer.py
    Spent hours using locally and now able to get last video uploaded(I think). Using an embed to easily store this information, will be working on temp thumbnail.

--- Youtube-Announcer/Youtube-Announcer.py
+++ Youtube-Announcer/Youtube-Announcer.py
@@ -16,29 +16,81 @@ class YTAnnouncer:
 
 	@commands.command()
 	async def update(self):
-		Get = []
+		List = []
+		Link = ""
+		#await self.bot.say("Get ready")
+		i = 0
+		print('Act')
 		"""This does stuff!"""
 		url = 'https://www.youtube.com/channel/UCJqiR6dpN3PqoNetKt-RB5w/videos'
 		async with aiohttp.get(url) as response:
 			soup = BeautifulSoup(await response.text(), "html.parser")
-		try:
-			print('Try')
-			for link in soup.find_all('a'):
-				print(link.get('href'))
-				for title in soup,find_all('title'):
-					print('Title ',title.get("")
-    				
-			#for link in soupObject.find_all(id='details'):
-				#Get =Get + link.get('title')
-        		#online = soupObject.find(id='contents').find_all('title').get_text()
-			#await self.bot.say(Get[1])
-		#except:
-			#await self.bot.say("Error.")
+		#try:
+		print('Try')
+		print("Links")
+		#32
+		def Make_List(Lists):
+			Link = ""
+			Video = ""
+			Dict = []
+			Dict2 = []
+			for var in Lists.find_all("a"):
+				Video = str(var.get("title"))
+				if Video == None or Video == "None" or Video == "":
+					Video = ""
+					#22
+					#32
+				Dict.append(Video)
+				Link = str(var.get("href"))
+				if Link == None or Link == "None" or Link == "":
+					Video = ""
 
-		#Your code will go here
-		await self.bot.say("I can do stuff!")
-		await self.bot.say(soupAvailable)
+				Dict2.append(Link)
+				#Link = str(var)
+				#print(Link)
+				"""if var != None or var != "":
+					for var2 in Lists.find(id="video_title"):
+						var2.get("video")
+						print(Link)"""
+			LatestLink = Dict2[32]
+			LatestVideo = Dict[32]
+			Main = 'https://www.youtube.com'
+			LatestLink = Main + LatestLink
+			#print(LatestVideo)
+			#print("Videos \n",Dict[32:33])
+			#print("Links \n", Dict2[32:33])
+			Vid = ("Video: " + LatestVideo)
+			Lin = ("Link: " + LatestLink)
+			print("Video: ", LatestVideo)
+			print("Link: ", LatestLink)
+			Final = Vid, Lin
+			#await self.bot.say(Vid, Lin)
+			
+		
+
+			embed=discord.Embed(
+				title="Latest Upload!", 
+				description="Here it is:", 
+				color=0x207cee)
+			embed.set_author(
+				name="Dp Bot", 
+				icon_url='https://cdn.discordapp.com/attachments/365496580490395649/378066120420098048/dp_bot.png')
+			embed.set_thumbnail(
+				url='https://cdn.discordapp.com/attachments/365496580490395649/378066486620585997/youtube-icon.png')
+			embed.add_field(
+				name=Vid, 
+				value=Lin, 
+				inline=True)
+			return embed
+
+		Print = Make_List(soup)
+		#print(Print[0])
+		#print(Print[1])
+		#await self.bot.say(Print[0])
+		#await self.bot.say(Print[1])
+		await self.bot.say(embed=Print)
 
 def setup(bot):
 	bot.add_cog(YTAnnouncer(bot))
+
 
