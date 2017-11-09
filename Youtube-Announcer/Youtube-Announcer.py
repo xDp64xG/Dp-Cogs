b1bfd2fc0d2798c0d4f0bd810b2f4a3026ced87b
xDp64xG <brow.paul64@gmail.com>
2017-11-09 15:42:19 -0500

    Update Youtube-Announcer.py

--- Youtube-Announcer/Youtube-Announcer.py
+++ Youtube-Announcer/Youtube-Announcer.py
@@ -22,15 +22,13 @@ class YTAnnouncer:
 		#	else:
 		#		url = var
 		#		self.url = url"""
-		await self.bot.say("Update confirmed.")
 		List = []
 		Link = ""
 		#await self.bot.say("Get ready")
 		i = 0
 		print('Act')
-		url = 'https://www.youtube.com/channel/UCJqiR6dpN3PqoNetKt-RB5w/videos'
 		"""This does stuff!"""
-		#url = 'https://www.youtube.com/channel/UCJqiR6dpN3PqoNetKt-RB5w/videos'
+		url = 'https://www.youtube.com/channel/UCJqiR6dpN3PqoNetKt-RB5w/videos'
 		async with aiohttp.get(url) as response:
 			soup = BeautifulSoup(await response.text(), "html.parser")
 		#try:
@@ -40,7 +38,6 @@ class YTAnnouncer:
 		def Make_List(Lists):
 			Link = ""
 			Video = ""
-			img = ""
 			Dict = []
 			Dict2 = []
 			Dict3 = []
@@ -56,14 +53,16 @@ class YTAnnouncer:
 					Video = ""
 
 				Dict2.append(Link)
-				
-			for Vari in Lists.find_all("img"):
-				img = str(Vari.get("src"))
-				#await self.bot.say(img)
-			Dict3.append(img)
-			
-			Image = Dict3[0]
-			#await self.bot.say(Dict3)
+
+				for Vari in Lists.find_all("img"):
+					img = str(Vari.get("src"))
+					#print(img)
+					#await self.bot.say(img)
+
+					Dict3.append(img)
+
+			#The numbers set here picks the first video.
+				Image = Dict3[12]
 			LatestLink = Dict2[32]
 			LatestVideo = Dict[32]
 			Main = 'https://www.youtube.com'
@@ -76,23 +75,25 @@ class YTAnnouncer:
 			print("Video: ", LatestVideo)
 			print("Link: ", LatestLink)
 			Final = Vid, Lin
+			#await self.bot.say(Vid, Lin)
+			
+		
+
 			embed=discord.Embed(
- 				title="Latest Upload!", 
- 				description="Here it is:", 
- 				color=0x207cee)
- 			embed.set_author(
- 				name="Dp Bot", 
+				title="Latest Upload!", 
+				description="Here it is:", 
+				color=0x207cee)
+			embed.set_author(
+				name="Dp Bot", 
 				icon_url='https://cdn.discordapp.com/attachments/365496580490395649/378066120420098048/dp_bot.png')
 			embed.set_thumbnail(
- 				url=Image)
- 				embed.add_field(
- 				name=Vid, 
- 				value=Lin, 
- 				inline=True)
- 			return embed
-			#await self.bot.say(Vid, Lin)
-		
-		
+				url=Image)
+			embed.add_field(
+				name=Vid,
+				#url = Lin, 
+				value=Lin, 
+				inline=True)
+			return embed
 
 		Print = Make_List(soup)
 		#print(Print[0])
