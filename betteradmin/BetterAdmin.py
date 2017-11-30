c7c9f2e4eda0f57ee95b9288607c6ed5e2a8be0c
xDp64xG <brow.paul64@gmail.com>
2017-11-30 18:59:14 -0500

    Create BetterAdmin.py

---
+++ betteradmin/BetterAdmin.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+#This is only temporary#
+
+class Mycog:
+    """My custom cog that does stuff!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def mycom(self):
+        """This does stuff!"""
+
+        #Your code will go here
+        await self.bot.say("I can do stuff!")
+
+def setup(bot):
+    bot.add_cog(Mycog(bot))
