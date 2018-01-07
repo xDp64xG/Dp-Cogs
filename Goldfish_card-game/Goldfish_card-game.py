d373031c1768993e6e708f4e1399e7f730035826
xDp64xG <brow.paul64@gmail.com>
2018-01-07 12:50:20 -0500

    Fixed a small fix
    The fix(previous commit) was to seperate the users using a '|'(or anything that isn't blank space) as people with a name such as "Testing this" equaled to 2(in length) because of the empty space I use to seperate. The game should now ONLY play once 2 people do the same command. Also added in for first person, a message of how to join in.

--- Goldfish_card-game/Goldfish_card-game.py
+++ Goldfish_card-game/Goldfish_card-game.py
@@ -1,5 +1,3 @@
-from .utils.dataIO import dataIO
-import os
 import discord
 from discord.ext import commands
 
@@ -7,14 +5,6 @@ from discord.ext import commands
 import random
 
 FullDeck = ['1S', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JackS', 'QueenS', 'KingS', '1H', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JackH', 'QueenH', 'KingH', '1C', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JackC', 'QueenC', 'KingC', '1D', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JackD', 'QueenD', 'KingD']
-#print(len(FullDeck), " Length of FullDeck")
-#FullDeck = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
-
-
-
-
-
-
 
 class G_Card_Game:
 
@@ -54,15 +44,6 @@ class G_Card_Game:
     Player1Cards = []
     Player2Cards = []
 
-
-    #Set here a switch
-
-    #Set Player 1 and 2
-
-
-
-
-
     List2 = ["Start"]
     variable = random.shuffle(FullDeck)
 
@@ -86,7 +67,7 @@ class G_Card_Game:
 
     if IDs == " ":
 
-      await self.bot.say("Started the game, waiting for someone to join.")
+      await self.bot.say("Started the game, waiting for someone to join. Another player can do ``[p]start``, start being the prefix.")
       self.User1 = ctx.message.author
       #people = str(author.name)
       member_object = server.get_member(author.id)
